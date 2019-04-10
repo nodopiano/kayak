d5539ebe7c24e646aab0aefec4f2caba87b46c8b
Daniele Perot <perotdaniele@gmail.com>
2019-04-10 18:06:15 +0200

    Wordmove inside docker

--- docker/tools/wordmove.sh
+++ docker/tools/wordmove.sh
@@ -1,28 +1,27 @@
-# !/bin/bash
-if [ ! -f .env ]; then
-    echo "ERROR: .env file missing - please run:"
-    echo "  ./kayak init"
-    exit -1
-fi
+#!/bin/bash
 
+# Colors
+red="\e[0;91m"
+yellow="\e[0;33m"
+blue="\e[0;96m"
+reset="\e[0m"
+
+# If Movefile does not exist, create one
 if [ ! -f Movefile ]; then
-    echo "INFO: Movefile not fount. Ready to create one."
-    read -p "Please write the relative path of where the Movefile should stay: " MOVEFILE_PATH
-
     if [ ! -d $MOVEFILE_PATH ]; then
-        echo "ERROR: $MOVEFILE_PATH is not a valid directory."
+        echo -e "${red}ERROR${reset}: $MOVEFILE_PATH is not a valid directory."
         exit -1
     fi
 
-    if [ ! -f "$MOVEFILE_PATH/Movefile" ]; then
-        echo "INFO: Movefile not present in $MOVEFILE_PATH"
-        echo "Generating a brand new Movefile..."
+    if [ $MOVEFILE_PATH != "./" ] && [ $MOVEFILE_PATH != "." ]; then
+        if [ ! -f "$MOVEFILE_PATH/Movefile" ]; then
+            echo -e "${blue}INFO${reset}: Movefile not present in $MOVEFILE_PATH"
+            echo "Generating a brand new Movefile..."
+            cp Movefile.example "$MOVEFILE_PATH/Movefile" || exit -2
+        fi
+    else
         cp Movefile.example "$MOVEFILE_PATH/Movefile" || exit -2
     fi
-
-    ln -sf "$MOVEFILE_PATH/Movefile" ./Movefile || exit -3
 fi
-
-source .env
 
 wordmove "$@"
